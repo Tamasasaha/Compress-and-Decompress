066156715df6a463a976309f41400b69414d1037
Tamasa Saha <tamasa@ialoy.com>
2024-04-16 16:14:02 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import GUI.AppFrame;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public  static void main(String[] args){
+        AppFrame frame= new AppFrame();
+    }
+}
